63fe33f0d5e2884045d198495a3b0eb4c085f8ec
prashanthrelekar <prashanthrelekarv@gmail.com>
2018-12-14 16:01:08 +0530

    Removed API key

--- src/app/app-config.ts
+++ src/app/app-config.ts
@@ -1,6 +1,6 @@
 var appConfig = {
     BOOK_API_URL: "https://www.googleapis.com/books/v1/volumes?q=",
-    BOOk_API_KEY: "AIzaSyCaBNye9dnH4Ey4Wa3oQQxcCsO1EP7YbLo",
+    BOOk_API_KEY: "",
 
     BOOKS_MOCK_URL: "http://localhost:3000"
 }
